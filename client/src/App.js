e21b920bffb04417435862d5182536e073235ad9
sieun95 <92782143+sieun95@users.noreply.github.com>
2022-07-07 16:08:19 +0900

    feat: ToolBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import MainPage from "./pages/MainPage";
-import { Route, Routes } from "react-router-dom";
+import ToolBar from "./components/ToolBar";
 
 const App = () => {
   return (
     <div style={{ maxWidth: 600, margin: "auto" }}>
       <ToastContainer />
+      <ToolBar />
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/auth/register" element={<RegisterPage />} />
